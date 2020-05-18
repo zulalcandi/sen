2399dff99fb7d5acb26fd222615a640f7e7089a2
Zülal ÇANDI <65549092+zulalcandi@users.noreply.github.com>
2020-05-18 17:17:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var text1="BU HAYATTAKİ EN ÖNEMLİ VE EN DEĞERLİ ŞEY SENSİN";
+    document.getElementById("demo").innerHTML=text1;
+}
+function benimFonk(){
+    var text2="Sitemi ziyaret ettiğin için teşekkür ederim"
+    document.getElementById("iletisim").innerHTML=text2;
+}
